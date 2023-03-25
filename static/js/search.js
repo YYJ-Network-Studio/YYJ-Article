f95ec6658dc2cc24976311099b8dd842a51609e4
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-03-25 11:15:00 +0800

    Add article title or hashtag search function

---
+++ static/js/search.js
@@ -0,0 +1,27 @@
+function search() {
+    let keyword = document.getElementById("search").value.toLowerCase(); // Get <input type="text" id="search">
+    let articles = document.querySelectorAll(".article-list li"); // Get all li in .article-list
+  
+    articles.forEach(function(article) {
+      let title = article.querySelector("a").textContent.toLowerCase();
+      let hashtags = article.querySelectorAll(".hashtag"); // .querySelectorAll + for to get all .hashtags
+      let match = false;
+
+      // Search every title and hashtag
+      if (title.includes(keyword)) {
+        match = true;
+      } else {
+        hashtags.forEach(function(hashtag) {
+          if (hashtag.textContent.toLowerCase().includes(keyword)) {
+            match = true;
+          }
+        });
+      }
+      if (match) {
+        article.style.display = "table";
+      } else {
+        article.style.display = "none";
+      }
+    });
+  }
+  
