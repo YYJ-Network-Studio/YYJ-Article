71459812e0a913ba954c657cf808830fc1675d48
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-04-02 04:35:11 +0800

    Add change theme function to every pages

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,11 +1,19 @@
-const themeToggle = document.querySelector('#theme');
+const themeToggle = document.getElementById('theme');
+const originalColor = themeToggle.style.color;
 const body = document.querySelector('body');
 const articleList = document.querySelector('.article-list')
 const list = document.querySelectorAll('.article-list li')
+const container = document.querySelector('.container')
 
 themeToggle.addEventListener('click', () => {
+  if (themeToggle.style.color == 'white') {
+    themeToggle.style.color = 'gray'
+  } else {
+    themeToggle.style.color = 'white';
+  }
   body.classList.toggle('dark-mode');
   articleList.classList.toggle('dark-mode');
+  container.classList.toggle('dark-mode');
   for (let i = 0; i < list.length; i++) {
     list[i].classList.toggle('dark-mode');
   }
